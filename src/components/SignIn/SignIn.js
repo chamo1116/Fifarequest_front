bda7ad55b50218720b54e98735128e1137b85d6e
SEBASTIAN CHAMORRO <sebastianchamorro@MacBook-Pro-de-SEBASTIAN.local>
2019-01-21 18:56:17 -0500

    Add player component

--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -55,7 +55,7 @@ class SignIn extends React.Component {
 				        />
 				      </div>
 				      <div className="mv3">
-				        <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+				        <label className="db fw6 lh-copy f6" htmlFor="password">Name</label>
 				        <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
 				        type="password" 
 				        name="password"  
